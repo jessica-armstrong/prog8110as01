baa570652abcaf3c8b97b4be6c8e9515481e733e
Jessica Armstrong <jarmstrong5022@conestogac.on.ca>
2020-06-21 17:05:42 -0400

    adding all files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const Game = require("./Game");
+const Game = require("./Adventure");
 
 // Create a new express application instance
 const app = express();
